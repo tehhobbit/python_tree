4a81d964ba3f995b531cbf91ca567112e05bea83
Loke Berne <loke.berne@gmail.com>
2021-01-10 16:56:35 +0000

    Add some extra funtions

--- python_tree/tree.py
+++ python_tree/tree.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import copy
 from python_tree.node import Node, BaseNode
 from python_tree.exceptions import NoRootNode
 from typing import Optional, List, Callable
@@ -6,8 +7,9 @@ from typing import Optional, List, Callable
 
 class Tree:
     """ Represents a tree"""
-    def __init__(self):
-        self.root = None
+    def __init__(self, root_node: Optional[Node] = None):
+        self.root = root_node
+        self.node_ids: List[str] = []
 
     def add_node(self, node: Node, parent: Optional[Node] = None) -> Node:
         """ Add a node to the tree.
@@ -49,6 +51,15 @@ class Tree:
         return [node for node in self.all_nodes if fn(node)]
 
     def map(self, fn: Callable) -> Tree:
+        """ Apply fn to each node in the tree.
+
+        Args:
+            fn: Transformation to apply to each node.
+
+        Returns:
+            A tree with all nodes transformed.
+
+        """
         if not self.root:
             raise NoRootNode("There is no root node")
         [fn(node) for node in self.all_nodes]
@@ -57,6 +68,13 @@ class Tree:
     @property
     def all_nodes(self):
         return self._get_all_nodes(self.root)
+
+    def get_node(self, node_id: str) -> Node:
+        return next((x for x in self.all_nodes if x.node_id == node_id))
+
+    def subtree(self, node: Node):
+        new_root = copy.copy(node)
+        return Tree(root_node=new_root)
 
     def _get_all_nodes(self, root):
         yield root
